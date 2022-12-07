62f53a0b2ed3fa35b4e4419e75aa1655342ef870
ulszust <ulszust@gmail.com>
2022-12-07 21:10:15 +0100

    added MyRentals view

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import MyRentals from "./MyRentals";
 
 const home = () => {
   return (
     <div>
       <h1>HOME!!</h1>
+      <MyRentals></MyRentals>
     </div>
   );
 }
