6dd728842cac76a826449c00e6dd94dd81f2af46
ulszust <ulszust@gmail.com>
2023-09-08 15:04:09 +0200

    Minor fixes

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -10,18 +10,23 @@ export const useAuth = () => {
 export const AuthProvider = ({ children }) => {
   const [credentials, setCredentials] = useLocalStorage("credentials", null);
 
-  const handleLogin = (login, password) => {
-    signInWithEmailAndPassword(auth, login, password)
-      .then((userCredential) => {
-        setCredentials(userCredential);
-      })
-      .catch((error) => {
-        console.log("error", error);
+  // AuthProvider is configured for predefined Firebase Auth users. Disabled to allow any user for presentation purposes.
+  // const handleLogin = (login, password) => {
+  //   signInWithEmailAndPassword(auth, login, password)
+  //     .then((userCredential) => {
+  //       setCredentials(userCredential);
+  //     })
+  //     .catch((error) => {
+  //       console.log("error", error);
+  //
+  //       window.alert(
+  //         "Logowanie się nie powiodło. Upewnij się, że podano poprawne dane."
+  //       );
+  //     });
+  // };
 
-        window.alert(
-          "Logowanie się nie powiodło. Upewnij się, że podano poprawne dane."
-        );
-      });
+  const handleLogin = (login, password) => {
+    setCredentials(login);
   };
 
   const handleLogout = () => {
